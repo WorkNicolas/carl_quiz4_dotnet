7466dd3086a56b9cb697088bff2f6c9c527a3fde
Carl Nicolas Mendoza <cmendo33@my.centennialcollege.ca>
2026-04-07 11:08:11 -0400

    first commit

---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcMovie.Models;
+
+public class Movie
+{
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(60, MinimumLength = 3)]
+    public string? Title { get; set; }
+
+    [Display(Name = "Release Date")]
+    [DataType(DataType.Date)]
+    public DateTime ReleaseDate { get; set; }
+
+    [Required]
+    [StringLength(30)]
+    public string? Genre { get; set; }
+
+    [Required]
+    [Range(1, 100)]
+    [DataType(DataType.Currency)]
+    [Column(TypeName = "decimal(18, 2)")]
+    public decimal Price { get; set; }
+
+    [Required]
+    [StringLength(5)]
+    public string? Rating { get; set; }
+}
